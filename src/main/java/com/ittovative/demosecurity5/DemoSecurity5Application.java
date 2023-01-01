1866a6093218817ea63f0d3adad819fd9120433c
mahmoudshahin-it <mahmoudshahin.it@gmail.com>
2023-01-01 15:48:49 +0200

    Initial commit

---
+++ src/main/java/com/ittovative/demosecurity5/DemoSecurity5Application.java
@@ -0,0 +1,13 @@
+package com.ittovative.demosecurity5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSecurity5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSecurity5Application.class, args);
+	}
+
+}
